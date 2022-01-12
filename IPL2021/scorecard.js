c5c86cc49fe973e0e4f3693832ee919d41b15d80
Vishal Prajapati <52890952+Vishal01-git@users.noreply.github.com>
2022-01-12 14:51:44 +0530

    Added xlsx

--- IPL2021/scorecard.js
+++ IPL2021/scorecard.js
@@ -48,6 +48,32 @@ function extractMatchDetails(html) {
     }
 }
 
+function dirCreater(filePath) {
+  if (fs.existsSync(filePath) == false) {
+    fs.mkdirSync(filePath);
+  }
+}
+function excelWriter(filePath, json, sheetName) {
+  let newWB = xlsx.utils.book_new();
+  let newWS = xlsx.utils.json_to_sheet(json);
+  xlsx.utils.book_append_sheet(newWB, newWS, sheetName);
+  xlsx.writeFile(newWB, filePath);
+}
+// // json data -> excel format convert
+// // -> newwb , ws , sheet name
+// // filePath
+// read
+//  workbook get
+function excelReader(filePath, sheetName) {
+  if (fs.existsSync(filePath) == false) {
+    return [];
+  }
+  let wb = xlsx.readFile(filePath);
+  let excelData = wb.Sheets[sheetName];
+  let ans = xlsx.utils.sheet_to_json(excelData);
+  return ans;
+}
+
 
 module.exports = {
     AllScorecards: AllScorecards
