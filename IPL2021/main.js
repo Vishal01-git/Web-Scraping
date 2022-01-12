30e961dc6db71a4c61bc844bac3a185670e4960b
Vishal Prajapati <52890952+Vishal01-git@users.noreply.github.com>
2022-01-12 14:49:10 +0530

    Create IPL folder

--- IPL2021/main.js
+++ IPL2021/main.js
@@ -1,7 +1,13 @@
 const url = "https://www.espncricinfo.com/series/ipl-2021-1249214";
 const request = require('request');
 const cheerio = require('cheerio');
+const fs = require("fs");
+const path = require("path");
 const AllMatchObj = require("./AllMatches");
+
+const iplPath = path.join(__dirname, "IPL");
+dirCreator(iplPath);
+
 request(url, function(err,response,html){
     if (err) {
         console.log(err);
@@ -17,4 +23,10 @@ function extractLink(html) {
     let fulllink = "https://www.espncricinfo.com" + link;
     //console.log(fulllink);
     AllMatchObj.getAllMatchesLink(fulllink);
+}
+
+function dirCreator(filePath) {
+    if (fs.existsSync(filePath) == false) {
+        fs.mkdirSync(filePath);
+    }
 }
